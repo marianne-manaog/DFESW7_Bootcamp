efbd661dab3f40f2d851d390502115c94ff81142
Marianne Manaog <91789965+marianne-manaog@users.noreply.github.com>
2022-01-06 17:49:38 +0000

    Add calculator revisited java codes

---
+++ src/conditionals_calculator_revisited/CalculatorRevisited.java
@@ -0,0 +1,25 @@
+package conditionals_calculator_revisited;
+
+public class CalculatorRevisited {
+
+	public double divide(double num1, double num2) {
+		
+		double result = 0;
+		
+		if(num1 < num2) {
+			result = num1 / num2;
+			return result;
+			
+		} else {
+				System.out.println("The division cannot be performed.");
+				return result;
+			}
+	
+			
+			
+	}
+		
+		
+}
+	
+	
---
+++ src/conditionals_calculator_revisited/Runner.java
@@ -0,0 +1,12 @@
+package conditionals_calculator_revisited;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		CalculatorRevisited newDivision = new CalculatorRevisited();
+		System.out.println(newDivision.divide(10, 5));
+	
+	}
+
+}

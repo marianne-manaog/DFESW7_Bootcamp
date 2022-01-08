5e02266e92c58132e01edc78679dc28168f3ab8f
Marianne Manaog <91789965+marianne-manaog@users.noreply.github.com>
2022-01-08 02:41:48 +0000

    Added conditionals flowcharts code

---
+++ src/conditionals_flowcharts/Flowcharts.java
@@ -0,0 +1,25 @@
+package conditionals_flowcharts;
+
+public class Flowcharts {
+
+	int num1;
+	int num2;
+	boolean bool = true;
+	
+	public int someMethod(boolean bool, int num1, int num2) {
+		
+		if(bool) {
+			int sum = num1 + num2;
+			return sum;
+		} else {
+			int product = num1 * num2;
+			return product;
+		}
+		
+		
+		
+		
+		
+	}
+	
+}
---
+++ src/conditionals_flowcharts/Runner.java
@@ -0,0 +1,11 @@
+package conditionals_flowcharts;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Flowcharts f = new Flowcharts();
+		System.out.println(f.someMethod(false, 3, 5));
+
+	}
+
+}
